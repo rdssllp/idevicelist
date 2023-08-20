4607c8bb8be3f86e1272a4656ebced22be575821
rdsllp <rajeshdan50@gmail.com>
2023-08-20 19:01:02 +0530

    pushhhh

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,26 @@
 
-// /**
-//  * Retrieves all items from the dataset.
-//  * @returns {Item[]} - An array of all items.
-//  */
-// export const getAllData = (): Item[] => {
-//   return data;
-// };
+/**
+ * Retrieves all items from the dataset.
+ * @returns {Item[]} - An array of all items.
+ */
+export const getAllData = (): Item[] => {
+  return data;
+};
 
-// /**
-//  * Searches for items by their model.
-//  * @param {string} model - The model to search for.
-//  * @returns {Item[]} - An array of items that match the provided model.
-//  */
-// export const searchByModel = (model: string): Item[] => {
-//   return data.filter((item) => item.model === model);
-// };
+/**
+ * Searches for items by their model.
+ * @param {string} model - The model to search for.
+ * @returns {Item[]} - An array of items that match the provided model.
+ */
+export const searchByModel = (model: string): Item[] => {
+  return data.filter((item) => item.model === model);
+};
 
-// /**
-//  * Searches for items by their code.
-//  * @param {string} code - The code to search for.
-//  * @returns {Item[]} - An array of items that match the provided code.
-//  */
-// export const searchByCode = (code: string): Item[] => {
-//   return data.filter((item) => item.code === code);
-// };
+/**
+ * Searches for items by their code.
+ * @param {string} code - The code to search for.
+ * @returns {Item[]} - An array of items that match the provided code.
+ */
+export const searchByCode = (code: string): Item[] => {
+  return data.filter((item) => item.code === code);
+};
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -22,110 +22,16 @@ export type IDeviceType =
   | IAudio
   | IAccessory;
 
-export interface IANumberDetails {
-  ANumber: string;
-  description?: string;
-}
-
-export interface IWatchModelDetails {
-  models: string[];
-  caseMaterial: string;
-  caseSize: string;
-  finish: string;
-}
-
-export interface IAirPodsModelDetails {
-  models: string[];
-  color: string;
-}
-
-export interface IAirTagModelDetails {
-  models: string[];
-  packSize: string;
-}
-
-export interface IAppleTVModelDetails {
-  models: string[];
-  storage: string;
-  color: string;
-}
-
-export interface ISiriRemoteModelDetails {
-  models: string[];
-}
-
-export interface IHomePodModelDetails {
-  models: string[];
-  color: string;
-}
-
-export interface IPadModelDetails {
-  models: string[];
-  color: string;
-  storage: string;
-}
-
-export interface IPadAirModelDetails extends IPadModelDetails {}
-export interface IPadProModelDetails extends IPadModelDetails {}
-export interface IPadMiniModelDetails extends IPadModelDetails {}
-
-export interface IPhoneModelDetails extends IPadModelDetails {}
-export interface IPodTouchModelDetails extends IPadModelDetails {}
-
-export interface IMacModelDetails {
-  models: string[];
-  color: string;
-  storage: string;
-}
-
-export interface IMacMiniModelDetails extends IMacModelDetails {}
-export interface IMacStudioModelDetails extends IMacModelDetails {}
-export interface IMacProModelDetails extends IMacModelDetails {}
-export interface IMacBookAirModelDetails extends IMacModelDetails {}
-export interface IMacBookProModelDetails extends IMacModelDetails {}
-export interface IMacMiniModelDetails extends IMacModelDetails {}
-
-export interface IApplePencilModelDetails {
-  models: string[];
-}
-
-export interface ISmartKeyboardModelDetails {
-  models: string[];
-  ipadCompatibility: string[];
-}
-
-export type IModelDetails =
-  | IWatchModelDetails
-  | IAirPodsModelDetails
-  | IAirTagModelDetails
-  | IAppleTVModelDetails
-  | ISiriRemoteModelDetails
-  | IHomePodModelDetails
-  | IPadModelDetails
-  | IPadAirModelDetails
-  | IPadProModelDetails
-  | IPadMiniModelDetails
-  | IPhoneModelDetails
-  | IPodTouchModelDetails
-  | IMacModelDetails
-  | IMacMiniModelDetails
-  | IMacStudioModelDetails
-  | IMacProModelDetails
-  | IMacBookAirModelDetails
-  | IMacBookProModelDetails
-  | IMacMiniModelDetails
-  | IApplePencilModelDetails
-  | ISmartKeyboardModelDetails;
-
 export interface IDevice {
   type: IDeviceType;
-  ANumbers?: string[];
-  FCCIDs?: string[];
+  generation: string;
+  models: string[];
+  aNumbers: string[];
+  fccIds: string[];
   internalName: string;
-  identifiers?: string[];
-  color?: string;
-  models?: string[];
-  modelDetails?: IModelDetails[];
-  boootrom?: string;
-  generation?: string;
+  identifier: string;
+  color: string;
+  storage: string;
+  bootrom: string;
+  modelsSearchable: string;
 }
